54ac8fe6771199d314e1e4e055ae36c5b4857073
Fly Gor <447268514@qq.com>
2022-08-17 11:48:16 +0800

    搭建基础ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import './styles/style.css';
 
 function App() {
   return (
-    <div className="App">
-HI
+    <div className="water">
+    <h1>提醒喝水器</h1>
+    <h3>目标：2L</h3>
+    <div className="cup">
+        <div className="remained" id="remained">
+          <span id="liters"></span>
+          <small>剩余2L</small>
+        </div>
     </div>
-  );
+    <p className="text">选择你喝了几杯水</p>
+    <div className="cups">
+        return (
+          <div
+            className={"cup cup-small"}
+          >
+            250 ml
+          </div>
+        );
+    </div>
+    <button
+      className="reset"
+    >
+      重置
+    </button>
+  </div>
+);
 }
 
 export default App;
