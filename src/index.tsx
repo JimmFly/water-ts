0abcabc456cfa338a104b443d027cf23a42d9306
Fly Gor <447268514@qq.com>
2022-08-18 15:01:19 +0800

    try i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "jotai";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./i18n/i18n";
 import { MyGlobalStyle } from "./styles/styled";
 
 const root = ReactDOM.createRoot(
