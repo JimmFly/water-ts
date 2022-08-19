60da41522b0b94804723b3cadab7d3fbd332bae1
Fly Gor <447268514@qq.com>
2022-08-19 11:16:59 +0800

    添加浏览器语言判断

--- src/i18n/react-i18next.ts
+++ src/i18n/react-i18next.ts
@@ -1,21 +1,23 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import en from "./en.json";
-import zh from "./zh.json";
+import en_US from "./en.json";
+import zh_CN from "./zh.json";
 
-export const resources = {
-  en: {
-    translation: en,
-  },
-  zh: {
-    translation: zh,
-  },
+const resources = {
+  en: en_US,
+  zh: zh_CN,
 } as const;
 
-i18n.use(initReactI18next).init({
-  lng: "en",
-  resources,
-  interpolation: {
-    escapeValue: false, // not needed for react as it escapes by default
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "zh",
+    fallbackLng: "zh",
+    resources,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+export default i18n;
