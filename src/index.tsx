1bf363eb5eb8c12b09355b9425329d8903a94951
Fly Gor <447268514@qq.com>
2022-09-16 11:52:42 +0800

    delete

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { Provider } from "jotai";
 import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Clock from "./components/Clock";
+
 import "./i18n/react-i18next";
 import { MyGlobalStyle } from "./styles/styled";
 
@@ -13,7 +13,6 @@ root.render(
   <Suspense fallback="loading">
     <Provider>
       <MyGlobalStyle />
-      <Clock seconds={5000} />
       <App />
     </Provider>
   </Suspense>
